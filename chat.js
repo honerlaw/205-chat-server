a35b5d91f616acedaf62b77a7ced2949ee8229d4
Derek Honerlaw <honerlawd@gmail.com>
2016-02-04 22:38:42 -0500

    initial commit

---
+++ chat.js
@@ -0,0 +1,1 @@
+var m=[];Vertx.vertx().createHttpServer().requestHandler(function(r){m.push(r.params().get('m'));r.response().putHeader("Content-Type","text/html").end(m.join('<p>')+'<form><input name=m>')}).listen(8080)
